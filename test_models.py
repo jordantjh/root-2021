cdc6cf184dc7ae1ae672bd112ebe708837b5ca37
Jin How Tan (Allyis Inc) <v-jint@microsoft.com>
2021-01-19 08:35:44 -0800

    enhanced driver class test; OOP-ize main.py; reformatted the output UI

--- test_models.py
+++ test_models.py
@@ -10,8 +10,17 @@ def test_driver_class():
         driver.get_mph()
 
     # add a trip
-    miles = 13.2
-    hours = 0.166667
-    driver.add_trip(miles, hours)
-    assert driver.get_total_miles() == miles
-    assert driver.get_mph() == (miles/hours)
+    miles_1 = 13.2
+    hours_1 = 0.166667
+    driver.add_trip(miles_1, hours_1)
+    assert driver.get_total_miles() == miles_1
+    assert driver.get_mph() == (miles_1/hours_1)
+
+    # add a second trip
+    miles_2 = 2.8
+    hours_2 = 0.008
+    driver.add_trip(miles_2, hours_2)
+    total_miles = miles_1 + miles_2
+    total_hours = hours_1 + hours_2
+    assert driver.get_total_miles() == total_miles
+    assert driver.get_mph() == (total_miles/total_hours)
